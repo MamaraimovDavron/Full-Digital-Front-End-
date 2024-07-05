72f250e0d43486cb9a7c40eca96e13bc62ea5e0f
DavronMamaraimov <davronmamaraimov1996@gmail.com>
2024-07-05 20:11:40 +0500

    Task 2 done

---
+++ Day-15. 5.07.2024/Task 2/script.js
@@ -0,0 +1,23 @@
+const marks = [80, 100, 50, 10];
+let sum = 0;
+
+function calculateGrade(marks) {
+  for (let i = 0; i < marks.length; i++) {
+    sum += marks[i] / marks.length;
+  }
+  if (sum > 1 && sum < 59) {
+    console.log("F");
+  } else if (sum > 60 && sum < 69) {
+    console.log("D");
+  } else if (sum > 70 && sum < 79) {
+    console.log("C");
+  } else if (sum > 80 && sum < 89) {
+    console.log("B");
+  } else if (sum > 90 && sum < 100) {
+    console.log("A");
+  } else {
+    console.log("Bunday son yo`q");
+  }
+}
+
+calculateGrade(marks);
