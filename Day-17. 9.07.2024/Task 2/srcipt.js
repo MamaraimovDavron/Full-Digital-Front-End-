c3a9b30824c135a518f1d8b82d71fa4396e7a68e
Davron <davronmamaraimov1996@gmail.com>
2024-07-09 16:56:46 +0500

    Task 2 done

---
+++ Day-17. 9.07.2024/Task 2/srcipt.js
@@ -0,0 +1,29 @@
+let library = [
+  {
+    author: "Bill Gates",
+    title: "The Road Ahead",
+    readingStatus: true,
+  },
+
+  { author: "Walter Isaacson", title: "Steve Jobs", readingStatus: true },
+
+  {
+    author: "Suzanne Collins",
+    title: "Mockingjay: The Final Book of the Hunger Games",
+    readingStatus: false,
+  },
+];
+
+function showData(object) {
+  for (const book of object) {
+    console.log(
+      "Kitob muallifi: " + book.author,
+      "\n",
+      "Kitob nomi: " + book.title,
+      "\n",
+      "O`qish holati: " + book.readingStatus
+    );
+  }
+}
+
+showData(library);
